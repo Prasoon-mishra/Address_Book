19e2c25e9a67f16ef2c55fe40bb2422af7d12f25
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-21 23:17:20 +0530

    [Prasoon] Added addcess book Use case 1

---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book!!!!");
+            ////UC1 Create Contact
+            AddressBook addressBook = new AddressBook("Prasoon", "Mishra", "Near xyz", "Kanpur", "UP", 415896, 2004155615, "Prasu@gamil.com");
+            addressBook.display();
+        }
+    }
+}
