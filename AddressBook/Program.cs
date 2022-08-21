aac96f7fc70945984cc08bbb62ebe28eb873b38e
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-21 23:24:39 +0530

    [Prasoon] Added create new contact details

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,8 +5,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book!!!!");
-            ////UC1 Create Contact
-            AddressBook addressBook = new AddressBook("Prasoon", "Mishra", "Near xyz", "Kanpur", "UP", 415896, 2004155615, "Prasu@gamil.com");
+            //////UC1 Create Contact
+            //AddressBook addressBook = new AddressBook("Prasoon", "Mishra", "Near xyz", "Kanpur", "UP", 415896, 2004155615, "Prasu@gamil.com");
+            //addressBook.display();
+
+            ////UC2 Add new Contact
+            Console.WriteLine("Enter details to add a new contact");
+            Console.WriteLine("Enter First Name");
+            string FName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string LName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter City ");
+            string City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string State = Console.ReadLine();
+            Console.WriteLine("Enter ZipCode");
+            int ZipCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone number");
+            long PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email");
+            string Email = Console.ReadLine();
+            AddressBook addressBook = new AddressBook(FName, LName, Address, City, State, ZipCode, PhoneNumber, Email);
             addressBook.display();
         }
     }
